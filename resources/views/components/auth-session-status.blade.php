2b3b31ef7b825f4311420309766d7c923a32cbba
Tijani Usman <brainyworld10@gmail.com>
2021-02-11 10:21:00 +0100

    first Commit

---
+++ resources/views/components/auth-session-status.blade.php
@@ -0,0 +1,15 @@
+@props(['status'])
+
+@if ($status)
+<div class="alert alert-warning alert-dismissible fade show" role="alert">
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+    <strong>
+      {{ $status }}
+    </strong>
+  </div>
+
+@endif
+
+
---
+++ resources/views/components/auth-validation-errors.blade.php
@@ -0,0 +1,18 @@
+@props(['errors'])
+
+@if ($errors->any())
+<div class="alert alert-danger alert-dismissible fade show" role="alert">
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+    <strong>
+      <ul class="mt-3 list-disc list-inside text-sm text-red-600">
+          @foreach ($errors->all() as $error)
+              <li>{{ $error }}</li>
+          @endforeach
+      </ul>
+    </strong>
+  </div>
+@endif
+
+
